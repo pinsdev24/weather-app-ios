c2248d3e412bd3623f2776767b935c69e98c9671
pinsdev24 <pinsdev24@gmail.com>
2024-10-29 12:17:22 +0100

    Initial Commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Prestilien Pindoh on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weatherappTests/weatherappTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappTests.swift
+//  weatherappTests
+//
+//  Created by Prestilien Pindoh on 29/10/2024.
+//
+
+import Testing
+@testable import weatherapp
+
+struct weatherappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
